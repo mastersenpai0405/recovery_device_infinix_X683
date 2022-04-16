d2f32ac60a1668dde7c72c48983376e1d5b4087f
donsenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2022-04-16 16:25:00 +0800

    X683: minor changes

--- omni_X683.mk
+++ omni_X683.mk
@@ -25,15 +25,11 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
-    libpuresoftkeymasterdevice \
-    ashmemd_aidl_interface-cpp \
-    libashmemd_client
+    libpuresoftkeymasterdevice
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # Fastbootd
 PRODUCT_PACKAGES += \
